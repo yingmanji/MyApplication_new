a03845f5761504a81028a5d06e267aaf38ac59fa
yingmanji <2283438112@qq.com>
2017-03-03 09:41:31 +0800

    练习进度条

---
+++ app/src/main/java/com/example/a0_0/myapplication_new/Activity_progressBar.java
@@ -0,0 +1,37 @@
+package com.example.a0_0.myapplication_new;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import java.util.Random;
+
+public class Activity_progressBar extends Activity {
+    private ProgressBar pb;
+    private Random random;
+    private int curProgress;
+    private int randomValue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_bar);
+        pb=(ProgressBar)findViewById(R.id.pb);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true)
+                {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    randomValue=random.nextInt(10);
+                    curProgress+=randomValue;
+                    pb.setProgress(curProgress);
+                }
+            }
+        }).start();
+    }
+
+}
