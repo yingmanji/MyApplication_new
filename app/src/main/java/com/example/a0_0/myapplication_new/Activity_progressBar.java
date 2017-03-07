de53d17ccad04c7a771e108a16156751b7011b7f
樱满集0_0 <2283438112@qq.com>
2017-03-07 13:26:43 +0800

    添加Activity_test测试include的使用,含有少量EditText控件使用注释

--- app/src/main/java/com/example/a0_0/myapplication_new/Activity_progressBar.java
+++ app/src/main/java/com/example/a0_0/myapplication_new/Activity_progressBar.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.a0_0.myapplication_new.model.User;
 
@@ -19,47 +22,60 @@ public class Activity_progressBar extends Activity {
     private int randomValue;
     private Handler handler;
     private User user;
+    private Button button;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_bar);
         pb=(ProgressBar)findViewById(R.id.pb);
-        user=new User();
         random=new Random();
+        button=(Button)findViewById(R.id.button);
+        textView=(TextView)findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while(curProgress<100)
+                        {
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            randomValue=random.nextInt(10);
+                            curProgress+=randomValue;
+//                  pb.setProgress(curProgress);//在早期的Android studio中只有UI线程可以控制View控件
+                            Message msg=new Message();
+                            user=new User();
+                            msg.what=0x110;//要发送的地址
+                            msg.arg1=curProgress;//要发送的内容
+                            msg.obj=user;
+                            handler.sendMessage(msg);
+//                    handler.sendEmptyMessage(0x110);
+                        }
+                    }
+                }).start();
+
+            }
+        });
         handler=new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
                 if(msg.what==0x110)
                 {
+                    user=(User)msg.obj;
                     System.out.println("aaa:"+user.getUserName());
                     pb.setProgress(msg.arg1);//curProgress
+                    textView.setText(user.getUserName());
                 }
             }
         };
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while(true)
-                {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    randomValue=random.nextInt(10);
-                    curProgress+=randomValue;
-//                  pb.setProgress(curProgress);//在早期的Android studio中只有UI线程可以控制View控件
-                    Message msg=new Message();
-                    msg.what=0x110;//要发送的地址
-                    msg.arg1=curProgress;//要发送的内容
-                    handler.sendMessage(msg);
-//                    handler.sendEmptyMessage(0x110);
 
-                }
-            }
-        }).start();
     }
 
 }
