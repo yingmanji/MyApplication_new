a39556956636d0f311a6d3af959a56b7cc7f018e
樱满集0_0 <2283438112@qq.com>
2017-03-09 11:04:25 +0800

    添加activity_list和OnItemClick等事件,listView使用示例

---
+++ app/src/main/java/com/example/a0_0/myapplication_new/activity_list.java
@@ -0,0 +1,52 @@
+package com.example.a0_0.myapplication_new;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class activity_list extends Activity {
+    TextView textView2;
+    ListView listView;
+    String[] items={"北京市","上海市","广州市","深圳市","西安市","珠海市","汕头市","潮州市","江门市","成都市","重庆市"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        textView2=(TextView)findViewById(R.id.textView2);
+        listView=(ListView)findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //textView2.setText(items[position]);
+                //textView2.setText(((TextView)listView.getChildAt(position)).getText());
+                textView2.setText(((TextView)view).getText());
+            }
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(activity_list.this,items[position],Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+        listView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction()==MotionEvent.ACTION_DOWN)
+                 Toast.makeText(activity_list.this,"down",Toast.LENGTH_LONG).show();
+                else if(event.getAction()==MotionEvent.ACTION_UP)
+                    Toast.makeText(activity_list.this,"up",Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+    }
+}
