3698aacc701e6d2b622d36f5860a2948c54cea91
樱满集0_0 <2283438112@qq.com>
2017-03-13 02:16:52 +0800

    添加继承ListActivity来添加listView的方法，并加了很关键的备注

--- app/src/main/java/com/example/a0_0/myapplication_new/activity_list2.java
+++ app/src/main/java/com/example/a0_0/myapplication_new/activity_list2.java
@@ -1,6 +1,5 @@
 package com.example.a0_0.myapplication_new;
 
-import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,18 +8,21 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class activity_list2 extends ListActivity {
-    String[] items={"北京市","上海市","广州市","深圳市","重庆市","天津市","武汉市","西安市","沈阳市","长沙市","珠海市"};
-    TextView textView2;
+    private static final String[]items={"北京市","上海市","广州市","深圳市","重庆市","天津市","武汉市","西安市","沈阳市","长沙市","珠海市"};
+    private TextView textView2;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_list2);
+        setContentView(R.layout.activity_list_view2);
+        //textView必须放在setContentView之后
+        textView2=(TextView)findViewById(R.id.textView2);
         setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items));
     }
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        //textView2.setText(items[position]);
+        textView2.setText(items[position]);
     }
 }
