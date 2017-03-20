0d5fce5a381f684439a993f449f46eddf575a62e
樱满集0_0 <2283438112@qq.com>
2017-03-21 01:47:20 +0800

    添加ActivityActionBar,并加了showAsAction,可以运行但是会报错

--- app/src/main/java/com/example/a0_0/myapplication_new/ActivityActionBar.java
+++ app/src/main/java/com/example/a0_0/myapplication_new/ActivityActionBar.java
@@ -3,6 +3,9 @@ package com.example.a0_0.myapplication_new;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class ActivityActionBar extends Activity {
 
@@ -16,5 +19,19 @@ public class ActivityActionBar extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.actions,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.add:Toast.makeText(this,"add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.reset:Toast.makeText(this,"reset",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.about:Toast.makeText(this,"about",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
