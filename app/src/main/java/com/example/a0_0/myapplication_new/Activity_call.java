591611c641f5c1bd76ac6e293a28e6b234fd25a4
yingmanji <2283438112@qq.com>
2017-03-24 09:45:48 +0800

    添加Activity_call

---
+++ app/src/main/java/com/example/a0_0/myapplication_new/Activity_call.java
@@ -0,0 +1,24 @@
+package com.example.a0_0.myapplication_new;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class Activity_call extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call);
+    }
+    public void call_list(View view)
+    {
+        Intent intent=new Intent(this,activity_list.class);
+        startActivity(intent);
+    }
+    public void web(View view)
+    {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.baidu.com")));
+    }
+}
