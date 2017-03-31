61d0c66f3dbd95697ab1ae81936ac6830bbad136
yingmanji <2283438112@qq.com>
2017-03-31 09:39:30 +0800

    添加了ActivityFLAGA等，测试setFlag中的FLAG参数

--- app/src/main/java/com/example/a0_0/myapplication_new/Activity_FLAG_D.java
+++ app/src/main/java/com/example/a0_0/myapplication_new/Activity_FLAG_D.java
@@ -22,6 +22,7 @@ public class Activity_FLAG_D extends Activity {
         //intent.setFlags(Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);//a b c d b
         //intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);//a b c d b
         //intent.setFlags(Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);//a b c d b
+        //intent.setFlags(Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT);//a b c d b
         startActivity(intent);
     }
 }
