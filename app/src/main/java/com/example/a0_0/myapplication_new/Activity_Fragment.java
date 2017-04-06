96ab4aec10673c2a56cb136d8d1286fb8a94fb8d
樱满集0_0 <2283438112@qq.com>
2017-04-06 10:47:12 +0800

    添加Fragment的练习

--- app/src/main/java/com/example/a0_0/myapplication_new/Activity_Fragment.java
+++ app/src/main/java/com/example/a0_0/myapplication_new/Activity_Fragment.java
@@ -2,12 +2,29 @@ package com.example.a0_0.myapplication_new;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Activity_Fragment extends Activity {
-
+    TextView tv1,tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__fragment);
+        tv1=(TextView) findViewById(R.id.tv1);
+        tv2=(TextView) findViewById(R.id.tv2);
+    }
+    public void change(View view)
+    {
+        if(view.getId()==R.id.btn1)
+        {
+            tv1.setVisibility(view.VISIBLE);
+            tv2.setVisibility(View.INVISIBLE);
+        }
+        else if(view.getId()==R.id.btn3)
+        {
+            tv1.setVisibility(view.INVISIBLE);
+            tv2.setVisibility(View.VISIBLE);
+        }
     }
 }
