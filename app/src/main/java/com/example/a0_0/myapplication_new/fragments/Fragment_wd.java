b5960962b4af7a6c88a08cee50bac5cbcac8f857
yingmanji <2283438112@qq.com>
2017-04-14 09:41:31 +0800

    添加Activity_navi_bottom

---
+++ app/src/main/java/com/example/a0_0/myapplication_new/fragments/Fragment_wd.java
@@ -0,0 +1,30 @@
+package com.example.a0_0.myapplication_new.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.a0_0.myapplication_new.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment_wd extends Fragment {
+
+
+    public Fragment_wd() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_wd, container, false);
+    }
+
+}
