4b8147892b9e7e23817f59c6e0556f7f864c6107
yingmanji <2283438112@qq.com>
2017-04-20 09:40:22 +0800

    添加Activity_URL2

--- app/src/main/java/com/example/a0_0/myapplication_new/Activity_URL2.java
+++ app/src/main/java/com/example/a0_0/myapplication_new/Activity_URL2.java
@@ -2,7 +2,10 @@ package com.example.a0_0.myapplication_new;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.example.a0_0.myapplication_new.model.NetWorkTask;
 
 public class Activity_URL2 extends Activity {
     private TextView tv_data;
@@ -10,5 +13,11 @@ public class Activity_URL2 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__url2);
+        tv_data=(TextView) findViewById(R.id.tv_data);
+    }
+    public void getNetData(View view)
+    {
+        String url="https://www.baidu.com";
+        new NetWorkTask(tv_data).execute(url);
     }
 }
