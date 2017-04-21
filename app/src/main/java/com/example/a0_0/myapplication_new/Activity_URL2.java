bdb2a1d0d46658ae3c605233b7934ec01ff512e2
yingmanji <2283438112@qq.com>
2017-04-21 09:40:47 +0800

    给Activity_URL2添加了进度条

--- app/src/main/java/com/example/a0_0/myapplication_new/Activity_URL2.java
+++ app/src/main/java/com/example/a0_0/myapplication_new/Activity_URL2.java
@@ -3,21 +3,24 @@ package com.example.a0_0.myapplication_new;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.a0_0.myapplication_new.model.NetWorkTask;
 
 public class Activity_URL2 extends Activity {
     private TextView tv_data;
+    private ProgressBar pb_load;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__url2);
         tv_data=(TextView) findViewById(R.id.tv_data);
+        pb_load=(ProgressBar)findViewById(R.id.pb_load);
     }
     public void getNetData(View view)
     {
         String url="https://www.baidu.com";
-        new NetWorkTask(tv_data).execute(url);
+        new NetWorkTask(tv_data,pb_load).execute(url);
     }
 }
--- app/src/main/java/com/example/a0_0/myapplication_new/model/NetWorkTask.java
+++ app/src/main/java/com/example/a0_0/myapplication_new/model/NetWorkTask.java
@@ -1,9 +1,11 @@
 package com.example.a0_0.myapplication_new.model;
 
 import android.os.AsyncTask;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -15,13 +17,15 @@ import java.net.URL;
 
 public class NetWorkTask extends AsyncTask<String ,Integer,String> {
     private TextView tv_data;
-    public NetWorkTask(TextView tv_data)
+    private ProgressBar pb_load;
+    public NetWorkTask(TextView tv_data,ProgressBar pb_load)
     {
         this.tv_data=tv_data;
+        this.pb_load=pb_load;
     }
     @Override
     protected String doInBackground(String... params) {
-        try{
+       /* try{
             URL url=new URL("https://www.baidu.com");
             HttpURLConnection connection =(HttpURLConnection) url.openConnection();
             InputStreamReader inputStreamReader=new InputStreamReader(connection.getInputStream());
@@ -38,12 +42,39 @@ public class NetWorkTask extends AsyncTask<String ,Integer,String> {
         catch (Exception e)
         {
             return "error";
+        }*/
+        try {
+            URL url = new URL(params[0]);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream=connection.getInputStream();
+            int total=connection.getContentLength();
+            int downloadSize=0;
+            byte[] buf=new byte[1];
+            int in;
+            StringBuffer result=new StringBuffer();
+            while((in=inputStream.read(buf))!=-1)
+            {
+                for(int i=1;i<1000000000;i++)
+                {
+                    downloadSize=downloadSize+in;//计算文件已下载大小
+                    int progress=(int)((float)downloadSize/total*100);//计算已下载百分数
+                    publishProgress(progress);//显示已下载百分数
+                    result.append(new String(buf, 0, in));//将下载的数据添加到result中
+                }
+            }
+            return result.toString();
+        }
+
+        catch (Exception e)
+        {
+            return "error";
         }
     }
 
     @Override
     protected void onProgressUpdate(Integer... values) {
         super.onProgressUpdate(values);
+        pb_load.setProgress(values[0]);
     }
 
     @Override
