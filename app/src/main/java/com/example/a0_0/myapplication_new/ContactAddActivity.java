5bee907f0fae171bade866230918da388b41cdda
yingmanji <2283438112@qq.com>
2017-05-11 09:40:29 +0800

    添加ContactAddActivity

---
+++ app/src/main/java/com/example/a0_0/myapplication_new/ContactAddActivity.java
@@ -0,0 +1,59 @@
+package com.example.a0_0.myapplication_new;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.a0_0.myapplication_new.model.Contact;
+import com.example.a0_0.myapplication_new.sqlite.service.ContactService;
+import com.example.a0_0.myapplication_new.sqlite.service.ContactServiceImpl;
+
+public class ContactAddActivity extends Activity {
+    private EditText et_cname,et_cphone;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_add);
+        et_cname=(EditText)findViewById(R.id.et_cname);
+        et_cphone=(EditText)findViewById(R.id.et_cphone);
+    }
+    public void save(View view)
+    {
+        String cname=et_cname.getText().toString().trim();
+        String cphone=et_cname.getText().toString().trim();
+        if(cname.equals("")||cname==null)
+        {
+            Toast.makeText(this,"姓名不能为空！",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(cphone.equals("")||cphone==null)
+        {
+            Toast.makeText(this,"手机号不能为空！",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        ContactService service=new ContactServiceImpl(this);
+        long flag=service.add(new Contact(cname,cphone));
+        if(flag>0)
+        {
+            Toast.makeText(this,"添加成功！",Toast.LENGTH_SHORT).show();
+            setResult(0x112);
+            finish();
+        }
+        else
+        {
+            Toast.makeText(this,"添加失败！",Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void reset(View view)
+    {
+        et_cname.setText("");
+        et_cphone.setText("");
+    }
+    public void cancel(View view)
+    {
+        setResult(0x111);
+        finish();
+    }
+}
