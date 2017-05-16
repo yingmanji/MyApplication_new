564b42f1256e37e99d3a17c520818043331beea4
yingmanji <2283438112@qq.com>
2017-05-16 11:23:36 +0800

    添加MyResultService

--- app/src/main/java/com/example/a0_0/myapplication_new/Activity_StartService.java
+++ app/src/main/java/com/example/a0_0/myapplication_new/Activity_StartService.java
@@ -3,8 +3,12 @@ package com.example.a0_0.myapplication_new;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.a0_0.myapplication_new.service.MyResultService;
 import com.example.a0_0.myapplication_new.service.MyService;
 
 public class Activity_StartService extends Activity {
@@ -16,8 +20,22 @@ public class Activity_StartService extends Activity {
     }
     public void startService(View view)
     {
-        Intent intent=new Intent(this, MyService.class);
-        intent.putExtra("myKey","Service");
+        /*Intent intent=new Intent(this, MyService.class);
+        intent.putExtra("myKey","Service");*/
+        Intent intent=new Intent(this, MyResultService.class);
+        intent.putExtra("myRsKey","resultService");
+        intent.putExtra("myReceiver",receiver);//该方法本身不存在
         startService(intent);
     }
+    private ResultReceiver receiver=new ResultReceiver(new Handler()){
+        //重写onReceiveResult方法
+
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            if(resultCode==0x120&&resultData!=null)
+            {
+                Toast.makeText(Activity_StartService.this,resultData.getString("myKey","获取消息失败！"),Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 }
---
+++ app/src/main/java/com/example/a0_0/myapplication_new/service/MyResultService.java
@@ -0,0 +1,36 @@
+package com.example.a0_0.myapplication_new.service;
+
+import android.app.ActivityManager;
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/5/16.
+ */
+
+public class MyResultService extends IntentService {
+    public MyResultService()
+    {
+        super("resultService");
+    }
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            String strValue = intent.getStringExtra("myRskey");
+            ResultReceiver receiver = (ResultReceiver) intent.getExtras().get("myReceiver");
+            System.out.println("#"+strValue);
+            try {
+                Thread.sleep(2000);//使线程休眠2秒
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Bundle bundle = new Bundle();
+            bundle.putString("myKey", "这是Service返回来的信息");
+            receiver.send(0x120, bundle);
+        }
+    }
+}
