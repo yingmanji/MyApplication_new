8bfcf0626aa973aa341a3964232410ca30aae530
樱满集0_0 <2283438112@qq.com>
2017-05-26 15:09:10 +0800

    添加加载功能

--- app/src/main/java/com/example/a0_0/myapplication_new/Activity_DialogUtil.java
+++ app/src/main/java/com/example/a0_0/myapplication_new/Activity_DialogUtil.java
@@ -1,13 +1,34 @@
 package com.example.a0_0.myapplication_new;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.a0_0.myapplication_new.model.DialogUtil;
 
 public class Activity_DialogUtil extends Activity {
-
+    Dialog dialog=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__dialog_util);
+    }
+    public void loading(View view)
+    {
+        dialog= DialogUtil.loadingDialog(this);
+        if(dialog!=null&& !dialog.isShowing())
+        {
+            dialog.show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(dialog!=null&&dialog.isShowing())
+        {
+            dialog.dismiss();
+        }
     }
 }
--- app/src/main/java/com/example/a0_0/myapplication_new/model/DialogUtil.java
+++ app/src/main/java/com/example/a0_0/myapplication_new/model/DialogUtil.java
@@ -2,6 +2,9 @@ package com.example.a0_0.myapplication_new.model;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.view.Window;
+
+import com.example.a0_0.myapplication_new.R;
 
 /**
  * Created by Administrator on 2017/5/26.
@@ -10,6 +13,10 @@ import android.content.Context;
 public class DialogUtil {
     public static Dialog loadingDialog(Context context)
     {
-        Dialog dialog=new Dialog()
+        Dialog dialog=new Dialog(context);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);//弹出windows窗口(无标题)
+        dialog.setContentView(R.layout.loading_dialog_view);
+        dialog.setCancelable(false);//设置为弹出dialog不可点击其他地方取消
+        return dialog;
     }
 }
