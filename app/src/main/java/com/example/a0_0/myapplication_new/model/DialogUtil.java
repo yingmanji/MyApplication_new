08a584a0d0b55914bcc7712e162e9c8984543fae
yingmanji <2283438112@qq.com>
2017-05-26 09:39:20 +0800

    添加DialogUtil

---
+++ app/src/main/java/com/example/a0_0/myapplication_new/model/DialogUtil.java
@@ -0,0 +1,15 @@
+package com.example.a0_0.myapplication_new.model;
+
+import android.app.Dialog;
+import android.content.Context;
+
+/**
+ * Created by Administrator on 2017/5/26.
+ */
+
+public class DialogUtil {
+    public static Dialog loadingDialog(Context context)
+    {
+        Dialog dialog=new Dialog()
+    }
+}
